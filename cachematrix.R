9a609ef656e33d09ec447e0a91df926ae6c04dd6
Victor Lara <vm.lara@gmail.com>
2015-01-21 18:54:48 -0500

    Commiting solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## For the exercise I assume that the matrix supplied is always invertible. 
+## That means it is always a square numerical matrix whose determinant != 0.
+## Because of this assumption I will not be checking the matrix before using it.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    ## get/set a matrix and its inverse.
+    inverse <- NULL
+    get <- function() x
+    set <- function(y) {
+        x <<- y
+        inverse <<- NULL
+    }
+    setinverse <- function(i) inverse <<- i
+    getinverse <- function() inverse
+    list(get=get, 
+         set=set, 
+         setinverse=setinverse, 
+         getinverse=getinverse)
 }
 
-
-## Write a short comment describing this function
-
+## Return a matrix that is the inverse of 'x'
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inverse <- x$getinverse()
+    if (!is.null(inverse)) {
+        print("Returning cached data:")
+        inverse    
+    } else{
+        inverse <- solve(x$get(), ...)
+        x$setinverse(inverse)
+        inverse
+    }
 }
